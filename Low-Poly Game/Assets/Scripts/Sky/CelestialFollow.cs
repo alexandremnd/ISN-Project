1db23e815ebee9c89157decaa6f1293577b5728d
duffy-y <alexxandre.mnd@gmail.com>
2020-02-28 21:10:19 +0100

    HDRP 7.2.1 + Script Cycle Jour/Nuit
    Passage de l'HDRP 7.1.8 à 7.2.1 à cause d'un leak de mémoire dans le code d'Unity (provoque saturation de la RAM et crash de l'ordinateur). Création des deux scripts fonctionnels pour le fonctionnement du ciel, le code n'est pas complet.

---
+++ Low-Poly Game/Assets/Scripts/Sky/CelestialFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility.Math;
+
+public class CelestialFollow : MonoBehaviour
+{
+    [Header("Object References")]
+    [SerializeField] private Transform m_sun;
+    [SerializeField] private Transform m_moon;
+    [SerializeField] private Transform m_sunLight;
+    [SerializeField] private Transform m_moonLight;
+    [SerializeField] private Transform m_camera;
+
+    [Header("Parameters")]
+    [SerializeField] private float m_distanceBetweenCameraCelestial;
+
+    void Update()
+    {
+        m_sun.position = m_camera.position - (m_sunLight.forward * m_distanceBetweenCameraCelestial);
+        m_moon.position = m_camera.position - (m_moonLight.forward * m_distanceBetweenCameraCelestial);
+    }
+
+    public bool MoonBellowHorizon
+    {
+        get
+        {
+            return CheckBellowHorizon(m_moonLight.forward, 5f);
+        }
+    }
+
+    public bool SunBellowHorizon
+    {
+        get
+        {
+            return CheckBellowHorizon(m_sunLight.forward, 5f);
+        }
+    }
+
+    public bool CheckBellowHorizon(Vector3 dir, float angleThreshold)
+    {
+        return dir.y < 0 ? false : (90 - Mathf.Acos(dir.y) * Mathf.Rad2Deg) > angleThreshold ? true : false;
+    }
+}
---
+++ Low-Poly Game/Assets/Scripts/Sky/DayCycler.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class DayCycler : MonoBehaviour
+{
+    [Header("Objects References")]
+    [SerializeField] private Transform m_sunLight;
+
+    [Header("Day Cycler Settings")]
+    [SerializeField, ReadOnly] private string m_timeFormatted;
+    [SerializeField, ReadOnly] private float m_time;
+    [SerializeField, Tooltip("Set the hour and minute when the day starts.")] private Vector2 m_startTime = new Vector2(12, 0);
+    [SerializeField, Tooltip("Include the length of day and night.")] private float m_dayLength;
+
+    #region Properties|Time
+    public Vector2 TimeToVector()
+    {
+        Vector2 time = new Vector2();
+        time.x = Mathf.Floor(m_time);
+        time.y = Mathf.Floor((m_time - time.x) * 30 / 0.5f);
+        return time;
+    }
+
+    public string TimeToString()
+    {
+        Vector2 time = TimeToVector();
+        return time.x + "h" + time.y + "m";
+    }
+
+    public float VectorToTime(Vector2 timeVector)
+    {
+        float time = timeVector.x;
+        time += timeVector.y * 0.5f / 30;
+        return time;
+    }
+    #endregion
+
+    #region Methods|Rotation
+    public float Rotation(float time)
+    {
+        // Cette équation a été déterminée manuellement afin de représenter la rotation en fonction du temps.
+        float latitude = Mathf.Sin((time * Mathf.PI) / 12 - 1.7f);
+        float yAngle = -latitude * 90f;
+        return yAngle;
+    }
+    #endregion
+
+    private void Start()
+    {
+        m_time = VectorToTime(m_startTime);
+        m_timeFormatted = TimeToString();
+        m_sunLight.localRotation = Quaternion.Euler(0, Rotation(m_time), 0);
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+        UpdateRotation();
+    }
+
+    private void UpdateTime()
+    {
+        m_time += (Time.deltaTime * 24) / (m_dayLength * 60);
+        m_timeFormatted = TimeToString();
+    }
+
+    private void UpdateRotation()
+    {
+        // On inverse la rotation en y (rotation anti-horaire).
+        m_sunLight.Rotate(new Vector3(0, -(Time.deltaTime * 6) / m_dayLength, 0));
+    }
+
+    private void SetTime(float time)
+    {
+        m_time = time;
+        m_sunLight.localRotation = Quaternion.Euler(0, Rotation(time), 0);
+    }
+}
