7b15a073cda08e6f1e8ce980bceaefbb0205eb0b
duffy-y <alexxandre.mnd@gmail.com>
2020-03-06 19:22:03 +0100

    Ajout des Clouds ScriptableGameObject

---
+++ Low-Poly Game/Assets/Scripts/Sky/Data/WeatherPreset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "Weather Preset", menuName = "Game/Weather Preset")]
+public class WeatherPreset : ScriptableObject
+{
+    [Header("Weather parameters preset")]
+    public Weather[] weather;
+}
