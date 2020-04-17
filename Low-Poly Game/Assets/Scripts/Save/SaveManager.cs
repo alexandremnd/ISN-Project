05609c6ccdd928ee72b10828f0a3bfb272a99af3
Duffy-y <alexxandre.mnd@gmail.com>
2020-04-17 19:29:15 +0200

    Backup

--- Low-Poly Game/Assets/Scripts/Save/SaveManager.cs
+++ Low-Poly Game/Assets/Scripts/Save/SaveManager.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
 
+// Créer un fichier JSON dans le dossier Documents du système. Conserve le nom de la sauvegarde, les succès, 
+// temps de jeu et chemin vers la sauvegarde.
 public class SaveManager : MonoBehaviour
 {
     // Start is called before the first frame update
