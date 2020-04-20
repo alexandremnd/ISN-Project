326509dff00a1ede0da0904001495ca1477bcd05
Duffy-y <alexxandre.mnd@gmail.com>
2020-04-20 23:17:10 +0200

    Ajout General Panel + Interaction avec le script Settings

--- Low-Poly Game/Assets/Scripts/Settings/SettingsManager.cs
+++ Low-Poly Game/Assets/Scripts/Settings/SettingsManager.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
 
 public class SettingsManager : MonoBehaviour
 {
-    private static SettingsManager Instance;
+    [HideInInspector] public static SettingsManager Instance;
+    public Dictionary<string, dynamic> Settings
+    {
+        get
+        {
+            return m_settings;
+        }
+    }
+
     private Dictionary<string, dynamic> m_settings = new Dictionary<string, dynamic>();
+    private string m_documentGamePath;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +29,28 @@ public class SettingsManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(this);
+        }
+
+        m_documentGamePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+        m_documentGamePath += "\\My Games\\";
+        if (!Directory.Exists(m_documentGamePath))
+        {
+            Directory.CreateDirectory(m_documentGamePath);
+        }
+        m_documentGamePath += Application.productName;
+        if (!Directory.Exists(m_documentGamePath))
+        {
+            Directory.CreateDirectory(m_documentGamePath);
+        }
+
+        if (File.Exists(m_documentGamePath + "\\settings.json"))
+        {
+            string json = File.ReadAllText(m_documentGamePath + "\\settings.json");
+            m_settings = (Dictionary<string, dynamic>)JsonConvert.DeserializeObject(json);
+        }
+        else
+        {
+
         }
     }
 
@@ -36,5 +69,21 @@ public class SettingsManager : MonoBehaviour
     public void SetFOV(float value)
     {
         SetSettings<float>("fov", Mathf.RoundToInt(value));
+    }
+
+    public void SetCompass(FlexibleButton button)
+    {
+        SetSettings<bool>("compass", button.IsActive);
+    }
+
+    public void SetHUD(FlexibleButton button)
+    {
+        SetSettings<bool>("hud", button.IsActive);
+    }
+
+    private void OnApplicationQuit()
+    {
+        string json = JsonConvert.SerializeObject(m_settings);
+        File.WriteAllText(m_documentGamePath + "\\settings.json", json);
     }
 }
