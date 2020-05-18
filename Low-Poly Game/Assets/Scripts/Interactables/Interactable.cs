145476260a745010a8904211da3689abd05315c7
Duffy-y <alexxandre.mnd@gmail.com>
2020-05-18 15:40:40 +0200

    Renommage des dossiers

--- Low-Poly Game/Assets/Scripts/Interactables/Interactable.cs
+++ Low-Poly Game/Assets/Scripts/Interactables/Interactable.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
-using System.IO;
+using UnityEngine.Events;
 
-// Créer un fichier JSON dans le dossier Documents du système. Conserve le nom de la sauvegarde, les succès, 
-// temps de jeu et chemin vers la sauvegarde.
-public class SaveManager : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
+    [SerializeField] private UnityAction m_action;
+
     // Start is called before the first frame update
     void Start()
     {
