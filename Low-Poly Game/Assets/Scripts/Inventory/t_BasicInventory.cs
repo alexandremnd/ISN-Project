16f0d6664e9239e4561e2f9503acc23ac9fb5b6f
Duffy-y <alexxandre.mnd@gmail.com>
2020-05-23 13:30:57 +0200

    Ajout d'un inventaire classique
    L'inventaire est juste pour la phase de développement car il comporte déjà les items directement. (il faut l'utiliser juste pour tester et développer des modules pour le jeu, mais il faudra revoir les noms de fonctions pluis tard)

---
+++ Low-Poly Game/Assets/Scripts/Inventory/t_BasicInventory.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Cette classe s'occupe de gérer la "hotbar" du joueur (la barre en bas de l'écran en jeu).
+// Elle n'est pas sensé se trouver dans le jeu final, mais elle nous permet de tester et développer
+// des fonctionnalités qui doivent intéragir avec l'inventaire, ou alors que l'action dépend de l'outil en main.
+// Ainsi, vu que le jeu est actuellement en démo, je laisse ce script.
+public class t_BasicInventory : MonoBehaviour
+{
+    [Header("Inventory Settings")]
+    [SerializeField] private const int m_slotNumber = 7;
+    [SerializeField] private Item[] m_hotbarItem = new Item[m_slotNumber];
+    [SerializeField] private GameObject m_slotPrefab;
+
+    [Header("References")]
+    [SerializeField] private Transform m_hotbar;
+
+    private int m_index = 0;
+    private Image[] m_slotImage = new Image[m_slotNumber];
+
+    private Color32 activeColor = new Color32(42, 96, 140, 188);
+    private Color32 idleColor = new Color32(0, 0, 0, 107);
+
+    [System.Serializable]
+    private class Item
+    {
+        public string itemInternalName;
+        public string itemName;
+        public Sprite itemSprite;
+        public GameObject itemObject;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // On itère sur chaque objet contenu dans la variable m_hotbar, on crée un slot (emplacement) sur l'interface graphique
+        // On y affiche l'image associé à l'objet du slot en question.
+        for (int i = 0; i < m_hotbarItem.Length; i++)
+        {
+            GameObject slot = Instantiate(m_slotPrefab, m_hotbar);
+            Image itemImage = slot.transform.GetChild(0).GetComponent<Image>();
+            m_slotImage[i] = slot.GetComponent<Image>();
+
+            itemImage.enabled = false;
+
+            if (m_hotbarItem[i].itemSprite != null)
+            {
+                Debug.Log("image");
+                itemImage.enabled = true;
+                itemImage.sprite = m_hotbarItem[i].itemSprite;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int oldIndex = m_index;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            m_index++;
+            if (m_index >= m_hotbarItem.Length)
+            {
+                m_index = 0;
+            }
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            m_index--;
+            if (m_index < 0)
+            {
+                m_index = m_hotbarItem.Length - 1;
+            }
+        }
+        UpdateHotbar(oldIndex, m_index);
+    }
+
+    void UpdateHotbar(int oldActiveIndex, int newActiveIndex)
+    {
+        m_slotImage[oldActiveIndex].color = idleColor;
+        m_slotImage[newActiveIndex].color = activeColor;
+    }
+}
