11f49e530a6c6b053e525ee13098a22b83687fdd
Duffy <53657346+Duffy-y@users.noreply.github.com>
2020-05-26 17:38:08 +0200

    Inventory  (#9)
    * Ajout d'un inventaire classique
    
    L'inventaire est juste pour la phase de développement car il comporte déjà les items directement. (il faut l'utiliser juste pour tester et développer des modules pour le jeu, mais il faudra revoir les noms de fonctions pluis tard)
    
    * Inventaire basique terminé

---
+++ Low-Poly Game/Assets/Scripts/Player/Classes/Raycaster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    [SerializeField] private float m_maxInteractionDistance = 2f;
+
+    private Transform m_cameraTransform;
+    private RaycastHit m_hit;
+    private Interactive m_interactiveItem;
+
+    private Transform m_holdedGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cameraTransform = Camera.main.transform;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Settings.Instance.GetButtonDown("primaryAttack"))
+        {
+            switch (t_BasicInventory.Instance.GetItem().itemInternalName)
+            {
+                case "pickaxe":
+                    bool raycastState = Physics.Raycast(m_cameraTransform.position, m_cameraTransform.forward, out m_hit, m_maxInteractionDistance);
+                    if (raycastState && m_hit.transform.CompareTag("Interactive"))
+                    {
+                        m_interactiveItem = m_hit.transform.GetComponent<Interactive>();
+                        if (m_interactiveItem != null)
+                        {
+                            m_interactiveItem.Interact(this.transform, InteractionType.Attack);
+                        }
+                    }
+                    break;
+                case "grab":
+                    raycastState = Physics.Raycast(m_cameraTransform.position, m_cameraTransform.forward, out m_hit, m_maxInteractionDistance);
+                    if ( (raycastState && m_hit.transform.CompareTag("Entity")))
+                    {
+                        m_holdedGameObject = m_hit.transform;
+                    }
+                    else if (m_holdedGameObject != null)
+                    {
+
+                    }
+                    else
+                    {
+                        m_holdedGameObject = null;
+                    }
+                    break;
+            }
+        }
+    }
+}
